27910ba1e9e2ee603c1c4658355b90c8d12b9b55
Chenli <wxxcl0825@126.com>
2024-12-09 13:53:37 +0800

    Mix texture

--- assets/shaders/fragment.frag
+++ assets/shaders/fragment.frag
@@ -3,5 +3,15 @@
 out vec4 FragColor;
 in vec3 color;
 in vec2 uv;
-uniform sampler2D sampler;
-void main() { FragColor = texture(sampler, uv); }
+uniform sampler2D grassSampler;
+uniform sampler2D landSampler;
+uniform sampler2D noiseSampler;
+void main() { 
+    vec4 grassColor = texture(grassSampler, uv);
+    vec4 landColor = texture(landSampler, uv);
+    vec4 noiseColor = texture(noiseSampler, uv);
+    float weight = noiseColor.r;
+    // vec4 finalColor = grassColor * (1.0 - weight) + landColor * weight;
+    vec4 finalColor = mix(grassColor, landColor, weight); // weight是第二个参数的权重
+    FragColor = vec4(finalColor.rgb, 1.0); 
+}
--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@
 
 GLuint vao;
 Shader* shader = nullptr;
-Texture* texture = nullptr;
+Texture* grassTexture = nullptr;
+Texture* landTexture = nullptr;
+Texture* noiseTexture = nullptr;
 
 void OnResize(int width, int height) {
     GL_CALL(glViewport(0, 0, width, height));
@@ -132,23 +134,27 @@ void prepareVAO() {
     float positions[] = {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f,
     };
 
     float colors[] = {
         1.0f, 0.0f, 0.0f,
         0.0f, 1.0f, 0.0f,
         0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, 0.5f,
     };
 
     float uvs[] = {
         0.0f, 0.0f,
         1.0f, 0.0f, 
-        0.5f, 1.0f,
+        0.0f, 1.0f,
+        1.0f, 1.0f,
     };
 
     unsigned int indices[] = {
         0, 1, 2,
+        2, 1, 3
     };
 
     GLuint posVbo, colorVbo, uvVbo;
@@ -195,7 +201,9 @@ void prepareShader() {
 }
 
 void prepareTexture() {
-    texture = new Texture("assets/textures/noir.png", 0);
+    grassTexture = new Texture("assets/textures/grass.jpg", 0);
+    landTexture = new Texture("assets/textures/land.jpg", 1);
+    noiseTexture = new Texture("assets/textures/noise.png", 2);
 }
 
 void render() {
@@ -208,14 +216,16 @@ void render() {
 
     // shader->setFloat("time", glfwGetTime());    // vs, fs变量重名时, 合二为一
     // shader->setVector3("uColor", 0.3, 0.4, 0.5);
-    shader->setInt("sampler", 0);
+    shader->setInt("grassSampler", 0);
+    shader->setInt("landSampler", 1);
+    shader->setInt("noiseSampler", 2);
 
     // 绑定vao(选择几何信息)
     glBindVertexArray(vao);
 
     // 发出绘制指令
     // glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     
     shader->end();
 }
@@ -241,6 +251,7 @@ int main() {
     }
 
     app->destroy();
-    delete texture;
+    delete grassTexture;
+    delete landTexture;
     return 0;
 }
